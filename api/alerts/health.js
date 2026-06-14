2406d898f36e50d075008fddddc6875e8cf54cbe
Knoxmortis <knoxmortis@gmail.com>
2026-06-14 16:59:59 -0500

    Add alert API deployment diagnostics

--- api/alerts/health.js
+++ api/alerts/health.js
@@ -1,5 +1,7 @@
 const { json, queueDiagnostics } = require("../_alertQueue");
 
+const BUILD_MARKER = "HEALTH_DIAG_2026_06_14_PRIMAL_GLASS_TAP";
+
 function matchingEnvKeys() {
   return Object.keys(process.env)
     .filter((key) => /(UPSTASH|REDIS|KV|ALERT|TWITCH)/i.test(key))
@@ -16,6 +18,7 @@ module.exports = async function handler(req, res) {
     return json(res, 200, {
       ok: true,
       service: "alerts",
+      buildMarker: BUILD_MARKER,
       runtime: {
         nodeEnv: process.env.NODE_ENV || "",
         vercelEnv: process.env.VERCEL_ENV || "",
@@ -24,6 +27,11 @@ module.exports = async function handler(req, res) {
       diagnostics: await queueDiagnostics(),
     });
   } catch (error) {
-    return json(res, 500, { ok: false, service: "alerts", error: error.message });
+    return json(res, 500, {
+      ok: false,
+      service: "alerts",
+      buildMarker: BUILD_MARKER,
+      error: error.message,
+    });
   }
 };
