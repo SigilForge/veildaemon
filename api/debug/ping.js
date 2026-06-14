871199a2f89568f176d331a2282971f2207742d3
Knoxmortis <knoxmortis@gmail.com>
2026-06-14 16:59:59 -0500

    Add alert API deployment diagnostics

---
+++ api/debug/ping.js
@@ -0,0 +1,14 @@
+const { json } = require("../_alertQueue");
+
+module.exports = function handler(req, res) {
+  if (req.method !== "GET") {
+    res.setHeader("Allow", "GET");
+    return json(res, 405, { ok: false, error: "Method not allowed." });
+  }
+
+  return json(res, 200, {
+    ok: true,
+    marker: "PRIMAL_GLASS_TAP",
+    time: new Date().toISOString(),
+  });
+};
